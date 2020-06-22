a02f54f20b2d2003d19c1549236a55b3c5d15f4e
TheoSabot <theo@virutalbox.com>
2020-06-22 22:46:58 +0200

    setted up eslint and prettier

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from "react";
-import Header from "./header/header";
-import Footer from "./footer/footer";
-import "./App.css";
+import React from 'react';
+import Header from './header/header';
+import Footer from './footer/footer';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <div class="dashboard">
+      <div className="dashboard">
         <h1>Simple React App served by NGINX and Docker</h1>
       </div>
       <Footer />
